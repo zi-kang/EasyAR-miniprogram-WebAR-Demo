e160b6c673822ba5a20b017a623d8296bad6b9f3
huzikang <huzikang@sightp.com>
2019-08-13 09:57:58 +0800

    apikey请求方式沿用之前的方式

--- apiSecretType/pages/recognition/recognition.js
+++ apiSecretType/pages/recognition/recognition.js
@@ -4,10 +4,7 @@ Page({
     height: '360',
     status: false,
     scanStatus: 'none',
-    msg: "请点击识别图片",
-    canvasWidth: '10',
-    canvasHeight: '10',
-    isRequest: false
+    msg: "请点击识别图片"
   },
 
   onLoad: function (options) {
@@ -15,9 +12,9 @@ Page({
 
     wx.getSystemInfo({
       success: res => {
-        this.setData({ height: res.windowHeight * 0.8 });
-      }
-    });
+      this.setData({ height: res.windowHeight * 0.8 });
+  }
+  });
   },
 
   stopScan: function () {
@@ -33,79 +30,50 @@ Page({
   },
 
   searchPhoto: function (filePath) {
+    console.log(filePath);
     wx.uploadFile({
       url: '您的请求接口',
       filePath,
       name: 'image',
       success: res => {
-        this.status = false;
-        let msg = JSON.parse(res.data);
-        that.setData({isRequest: false});
-        if (msg.statusCode == 0) {
-          this.setData({ msg: '识别成功' });
-          setTimeout(() => {
-            console.info('go to webar');
-            wx.navigateTo({
-              url: '../show/show'
-            });
-          }, 500);
-        }
-      },
-      fail: err => {
-        this.status = false;
-        this.setData({ msg: JSON.stringify(err), isRequest: false });
-      }
-    });
-  },
+      this.status = false;
 
-  transformArrayBufferToFilePath: function(frame){
-    var that = this;
-    const data = new Uint8ClampedArray(frame.data);
-    this.setData({canvasWidth: frame.width, canvasHeight: frame.height, isRequest: true});
-    wx.canvasPutImageData({
-      canvasId: 'firstCanvas',
-      x: 0,
-      y: 0,
-      width: frame.width,
-      height: frame.height,
-      data: data,
-      success(res) {
-        wx.canvasToTempFilePath({
-          x: 0,
-          y: 0,
-          width: frame.width,
-          height: frame.height,
-          canvasId: 'firstCanvas',
-          success(res) {
-            that.searchPhoto(res.tempFilePath)
-          }
-        })
-      },
-      fail(err) {
-        that.setData({isRequest: false});
-      }
-    });
+    let msg = JSON.parse(res.data);
+    if (msg.statusCode != 0) {
+      this.setData({ msg: '未识别到目标，请点击屏幕继续识别' });
+    } else {
+      this.setData({ msg: '识别成功' });
+      setTimeout(() => {
+        console.info('go to webar');
+      wx.navigateTo({
+        url: '../show/show'
+      });
+    }, 500);
+    }
+  },
+    fail: err => {
+      this.status = false;
+      this.setData({ msg: JSON.stringify(err) });
+    }
+  });
   },
 
   takePhoto: function (e) {
+    console.log(111);
     if (this.status) return;
 
     this.status = true;
 
-    const context = wx.createCameraContext()
-
-    this.listener = context.onCameraFrame((frame) => {
-      if(!this.data.isRequest) {
-        this.transformArrayBufferToFilePath(frame);
-      }
-    })
-    this.listener.start({
+    this.ctx.takePhoto({
+      quality: 'normal',
       success: res => {
-        this.setData({ msg: '识别中...' });
-      },
-      fail: err => {
-        this.setData({ msg: err});
-      }
-    })
+      this.setData({ msg: '识别中...' });
+    this.searchPhoto(res.tempImagePath)
+  },
+    fail: err => {
+      this.stopScan();
+      this.setData({ msg: '未识别到目标' });
+    }
+  });
   }
 })
